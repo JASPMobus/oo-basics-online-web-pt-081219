be22fa78ee4165097c677cc4f61dcafda7f9e447
James Mobus <mobusj@live.com>
2019-08-30 17:31:52 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,4 +1,4 @@
 # Make your shoe class here!
 class Shoe 
   attr_accessor :brand, :color, :size, :material, :condition 
-  attr_writer :brand, :color, :size, :material, :condition 
+  attr_writer :brand, :color, :size, :material, :condition
