6c78df33390806ffcf7114856aa275cbc4c478fb
James Mobus <mobusj@live.com>
2019-08-30 17:32:27 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,3 +2,12 @@
 class Shoe 
   attr_accessor :brand, :color, :size, :material, :condition 
   attr_writer :brand, :color, :size, :material, :condition
+  
+  def initialize(brand)
+    @brand = brand
+  end 
+  
+  def cobble 
+    puts "The shoe has been repaired"
+  end 
+end
