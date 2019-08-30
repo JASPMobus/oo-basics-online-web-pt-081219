ee5925910945f30297cb1c7ce7c2cbca10221a71
James Mobus <mobusj@live.com>
2019-08-30 17:30:07 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,8 +1,9 @@
 class Book
-  attr_accessor :title, :author_name, :page_count, :genre
+  attr_accessor :title, :author, :page_count, :genre
   
-  def initialize(title)
-    @title = title 
+  def initialize(title, author)
+    @title = title
+    @author = author
   end 
   
   def turn_page 
